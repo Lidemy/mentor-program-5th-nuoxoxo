29575e66d1f9a8b2c583319940b836737a605cf4
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2021-05-04 00:08:37 +0200

    Update hw3.js

--- homeworks/week4/hw3.js
+++ homeworks/week4/hw3.js
@@ -14,15 +14,16 @@ function lookup(q) {
       return console.log(err)
     }
     let data = JSON.parse(body)
-    for (let i = 0; i < data.length; i++){
-      let co = data[i].name
-      let ca = data[i].capital
-      let cu = data[i].currencies[0].code
-      let cc = data[i].callingCodes[0]
-      console.log(
-        `============\n國家：${co}\n首都：${ca}\n貨幣：${cu}\n國碼：${cc}`
-      )
+    if (data.status === 404) {
+      return console.log('找不到國家資訊')
+    } else {
+      for (let i = 0; i < data.length; i++){
+        let co = data[i].name
+        let ca = data[i].capital
+        let cu = data[i].currencies[0].code
+        let cc = data[i].callingCodes[0]
+        console.log(`============\n國家：${co}\n首都：${ca}\n貨幣：${cu}\n國碼：${cc}`)
+      }
     }
   })
 }
-
