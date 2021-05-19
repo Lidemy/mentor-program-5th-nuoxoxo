7aadd7740c37f424a55bb204098057d55282fa79
nuo <49533950+nuoxoxo@users.noreply.github.com>
2021-05-19 08:59:24 +0200

    update: typo fix

--- homeworks/week2/challenge2.js
+++ homeworks/week2/challenge2.js
@@ -1,14 +1,14 @@
-function search(arr, n, low = 0, high = arr.length - 1){
- 
-  if (low > high){
+function search(arr, n, low = 0, high = arr.length - 1) {
+  
+  if (low > high) {
     return (-1);
   }
-
+  
   let midpoint = Math.floor((low + high) / 2);
-
-  if (arr[midpoint] === n){
+  
+  if (arr[midpoint] === n) {
     return midpoint;
-  } else if (arr[midpoint] > n){
+  } else if (arr[midpoint] > n) {
     return search(arr, n, low, midpoint - 1);
   } else {
     return search(arr, n, midpoint + 1, high);
